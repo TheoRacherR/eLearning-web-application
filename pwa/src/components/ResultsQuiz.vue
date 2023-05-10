a7e1cd2ef3f2ee5103bb5edc2d49d79ee60dcb34
Vaalgtir <e.eniona2@gmail.com>
2023-05-10 18:02:48 +0200

    feat: initi route

--- pwa/src/components/ResultsQuiz.vue
+++ pwa/src/components/ResultsQuiz.vue
@@ -1,4 +1,74 @@
-<script setup></script>
+<script setup>
+import axios from "axios";
+import router from "../router";
+import { computed, onMounted, ref, watchEffect } from "vue";
+import { store } from "../store/store";
+
+const { id: courseId } = router.currentRoute.value.params;
+
+const courses = ref({});
+const questions = ref({});
+
+const course = computed(() => {
+  return store.courses.selected ? courses.value[store.courses.selected] : {};
+});
+
+watchEffect(() => {
+  courses.value = store.courses.list;
+});
+
+watchEffect(() => {
+  if (store.user.token && courses.value && course.value) {
+    questions.value =
+      Object.values(JSON.parse(course.value.sequence).questions) || [];
+
+    axios
+      .get(import.meta.env.VITE_API_URL + "/answers", {
+        headers: {
+          Authorization: `Bearer ${store.user.token}`,
+        },
+      })
+      .then(({ data: { ["hydra:member"]: answerRaw } }) => {
+        const userAnswers = answerRaw.filter((item) => {
+          console.log("debug", item.account, "/users/" + store.user.id);
+
+          const arrayQuestions = Object.values(questions.value).map((item) =>
+            parseInt(item.questionId)
+          );
+          return (
+            item.account === "/users/" + store.user.id &&
+            arrayQuestions.includes(parseInt(item.question.split("/")[2]))
+          );
+        });
+
+        const body = {};
+
+        userAnswers.map((item) => {
+          body[item.question.split("/")[2]] = item.value;
+        });
+
+        console.log("debug", body);
+
+        axios.post(import.meta.env.VITE_API_URL + "/quiz/submit", body, {
+          headers: {
+            Authorization: `Bearer ${store.user.token}`,
+          },
+        });
+      });
+  }
+});
+
+onMounted(() => {
+  // checkConnection(false, true, "Quiz");
+  store.selectCourse(courseId);
+  // if (
+  //   typeof course.value?.possessed === "boolean" &&
+  //   !course.value?.possessed
+  // ) {
+  //   router.push(`/detail/${courseId}`);
+  // }
+});
+</script>
 
 <template>
   <div class="wrapper">Vos résultats arrivent</div>
