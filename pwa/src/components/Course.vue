1e7143257b32b6049a173d335d43b1a324857f67
Vaalgtir <e.eniona2@gmail.com>
2023-05-12 09:05:20 +0200

    fix: fix butotn quiz

--- pwa/src/components/Course.vue
+++ pwa/src/components/Course.vue
@@ -11,6 +11,7 @@ const courses = ref({});
 const chapter = ref({});
 const chapIndex = ref({});
 const chapters = ref({});
+const questions = ref({});
 
 const course = computed(() => {
   return store.courses.selected ? courses.value[store.courses.selected] : {};
@@ -22,9 +23,9 @@ watch(
   () => {
     location.reload();
   }
-  );
+);
 
-  watchEffect(() => {
+watchEffect(() => {
   courses.value = store.courses.list;
 });
 
@@ -37,6 +38,9 @@ watchEffect(() => {
     chapIndex.value = chapIndexRaw || 0;
     chapters.value =
       Object.values(JSON.parse(course.value.sequence).chapters) || [];
+    questions.value =
+      Object.values(JSON.parse(course.value.sequence).questions) || [];
+    console.log("debug", questions.value);
     chapter.value = chapters.value[chapIndex.value];
 
     editor.value.setHTML(chapter.value?.content);
@@ -46,59 +50,64 @@ watchEffect(() => {
 onMounted(() => {
   checkConnection(false, true, false, false, "Course page");
   store.selectCourse(courseId);
-  
+
   if (
     typeof course.value?.possessed === "boolean" &&
     !course.value?.possessed
-    ) {
-      router.push(`/detail/${courseId}`);
-    }
-  });
+  ) {
+    router.push(`/detail/${courseId}`);
+  }
+});
 </script>
 
 <template>
   <div class="wrapper-course-content">
     <h1>{{ course?.title }}</h1>
-    <h2>Chapitre n°{{ parseInt(chapIndex)+1 }}: "{{ chapter?.title }}"</h2>
+    <h2>Chapitre n°{{ parseInt(chapIndex) + 1 }}: "{{ chapter?.title }}"</h2>
 
     <div class="group-buttons">
-    <button class="bttn bttn-wng quiz-btn" v-if="parseInt(chapIndex) > 0">
-      <RouterLink :to="`/course/${courseId}/${parseInt(chapIndex) - 1}`">
-        <va-icon name="chevron_left" />
-        Chapitre précédent
-      </RouterLink>
-    </button>
-    
-    <button class="bttn bttn-drk quiz-btn go-quiz" v-if="chapters.length === parseInt(chapIndex) + 1">
-      <RouterLink :to="`/course/${courseId}/quiz`">
-        <va-icon name="quiz" />
-        Faire le Quiz
-        <va-icon name="chevron_right" />
-      </RouterLink>
-    </button>
-    
-    <button class="bttn bttn-wng quiz-btn next-chapter" v-else>
-      <RouterLink :to="`/course/${courseId}/${parseInt(chapIndex) + 1}`">
-        Chapitre suivant
-        <va-icon name="chevron_right" />
-      </RouterLink>
-    </button>
+      <button class="bttn bttn-wng quiz-btn" v-if="parseInt(chapIndex) > 0">
+        <RouterLink :to="`/course/${courseId}/${parseInt(chapIndex) - 1}`">
+          <va-icon name="chevron_left" />
+          Chapitre précédent
+        </RouterLink>
+      </button>
+
+      <button
+        class="bttn bttn-drk quiz-btn go-quiz"
+        v-if="
+          chapters.length === parseInt(chapIndex) + 1 && questions.length > 0
+        "
+      >
+        <RouterLink :to="`/course/${courseId}/quiz`">
+          <va-icon name="quiz" />
+          Faire le Quiz
+          <va-icon name="chevron_right" />
+        </RouterLink>
+      </button>
+
+      <button
+        class="bttn bttn-wng quiz-btn next-chapter"
+        v-else-if="chapters.length !== parseInt(chapIndex) + 1"
+      >
+        <RouterLink :to="`/course/${courseId}/${parseInt(chapIndex) + 1}`">
+          Chapitre suivant
+          <va-icon name="chevron_right" />
+        </RouterLink>
+      </button>
     </div>
 
     <QuillEditor ref="editor" :readOnly="true" :toolbar="[]" theme="snow" />
-
-  
-
   </div>
 </template>
 
 <style lang="scss" scoped>
 .wrapper-course-content {
   // text-align: center;
-  
+
   h1 {
     padding: 3vh 30vw 3vh 30vw;
-    text-align:left;
+    text-align: left;
     background-color: rgb(168, 156, 139);
   }
   h2 {
@@ -109,15 +118,16 @@ onMounted(() => {
     padding: 3vh 30vw 3vh 30vw;
     display: flex;
     // justify-content: space-between;
-  
+
     button {
-      a{
+      a {
         text-decoration: none;
         color: white;
       }
     }
-  
-    button.next-chapter, button.go-quiz{
+
+    button.next-chapter,
+    button.go-quiz {
       margin: 0 0 0 auto;
     }
   }
